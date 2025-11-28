9a9e707c0a9afb198df3d47a59c4c5e76c29af11
Leo Benítez <141597954+fk2731@users.noreply.github.com>
2025-11-27 23:35:20 -0600

    Add TypeScript checking and Tailwind CSS plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,7 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   site: "https://fk2731.github.io/",
   vite: {
